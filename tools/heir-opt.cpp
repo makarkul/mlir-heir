078d72b762b003e6c2325eb33c59b0dc62c74541
Makarand Kulkarni <makarand.kulkarni@saankhyalabs.com>
2024-05-31 18:58:30 +0530

    Add default passes and tests to check the lowering of math functions to llvm dialect

--- tools/heir-opt.cpp
+++ tools/heir-opt.cpp
@@ -1,9 +1,13 @@
 #include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
 
 int main(int argc, char **argv) {
   mlir::DialectRegistry registry;
   mlir::registerAllDialects(registry);
+
+  mlir::registerAllPasses();
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "HEIR Pass Driver", registry)
