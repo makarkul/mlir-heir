1b535f5a8c3e69c094ca386a510578f0b07f00fa
Makarand Kulkarni <makarand.kulkarni@saankhyalabs.com>
2024-06-03 09:42:00 +0530

    Add pass shell to heir-opt binary

---
+++ lib/Transform/Affine/AffineFullUnroll.cpp
@@ -0,0 +1,10 @@
+#include "lib/Transform/Affine/AffineFullUnroll.h"
+
+namespace mlir {
+  namespace heir {
+
+    void AffineFullUnrollPass::runOnOperation() {
+      return;
+    }
+  } // namespace tutorial
+} // namespace mlir
--- tools/heir-opt.cpp
+++ tools/heir-opt.cpp
@@ -1,5 +1,8 @@
+#include "lib/Transform/Affine/AffineFullUnroll.h"
 #include "mlir/InitAllDialects.h"
 #include "mlir/InitAllPasses.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Pass/PassRegistry.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
 
@@ -8,6 +11,8 @@ int main(int argc, char **argv) {
   mlir::registerAllDialects(registry);
 
   mlir::registerAllPasses();
+
+  mlir::PassRegistration<mlir::heir::AffineFullUnrollPass>();
 
   return mlir::asMainReturnCode(
       mlir::MlirOptMain(argc, argv, "HEIR Pass Driver", registry)
