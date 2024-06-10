7fddd157f16b4e3dd5ab5edd1c3c7e72f7d8d420
Makarand Kulkarni <makarand.kulkarni@saankhyalabs.com>
2024-06-10 16:25:40 +0530

    Add a poly type

--- lib/Dialect/Poly/PolyDialect.cpp
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -1,6 +1,13 @@
 #include "lib/Dialect/Poly/PolyDialect.h"
 
+#include "lib/Dialect/Poly/PolyTypes.h"
+#include "mlir/IR/Builders.h"
+#include "llvm/ADT/TypeSwitch.h"
+
 #include "lib/Dialect/Poly/PolyDialect.cpp.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Poly/PolyTypes.cpp.inc"
 
 namespace mlir {
   namespace heir {
@@ -8,7 +15,11 @@ namespace mlir {
 
       void PolyDialect::initialize() {
         // This is where we will register types and operations with the dialect
-      }
-    }
-  }
-}
+        addTypes<
+#define GET_TYPEDEF_LIST
+#include "lib/Dialect/Poly/PolyTypes.cpp.inc"
+          >();
+      } 
+    }// namespace poly 
+  }// namespace heir
+}// namespace mlir
---
+++ tests/poly_syntax.mlir
@@ -0,0 +1,7 @@
+// RUN: heir-opt %s
+
+module {
+  func.func @main(%arg0: !poly.poly) -> !poly.poly {
+    return %arg0 : !poly.poly
+  }
+}
